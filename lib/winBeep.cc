5532cb57e2bc5dc99a1bde19e3fd5a497ab2883b
gnomesley <gnomesley@gmail.com>
2020-05-25 04:46:55 -0400

    Fix for newer versions of node

--- lib/winBeep.cc
+++ lib/winBeep.cc
@@ -13,30 +13,30 @@ static void WinBeep(const FunctionCallbackInfo<Value> &args)
   {
     if (!args[0]->IsNumber() || !args[1]->IsNumber())
     {
-      isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Arguments type must be number.")));
+      isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Arguments type must be number.").ToLocalChecked()));
       return;
     }
-    int frequency = args[0]->Int32Value();
-    int duration = args[1]->Int32Value();
+    int frequency = args[0]->Int32Value(isolate->GetCurrentContext()).ToChecked();
+    int duration = args[1]->Int32Value(isolate->GetCurrentContext()).ToChecked();
     if (frequency < 37 || frequency > 32767)
     {
-      isolate->ThrowException(Exception::RangeError(String::NewFromUtf8(isolate, "Frequency must be in 37 thru 32767.")));
+      isolate->ThrowException(Exception::RangeError(String::NewFromUtf8(isolate, "Frequency must be in 37 thru 32767.").ToLocalChecked()));
       return;
     }
     if (duration <= 0)
     {
-      isolate->ThrowException(Exception::RangeError(String::NewFromUtf8(isolate, "Duration must > 0.")));
+      isolate->ThrowException(Exception::RangeError(String::NewFromUtf8(isolate, "Duration must > 0.").ToLocalChecked()));
       return;
     }
     ok = Beep(frequency, duration);
     if (!ok)
     {
-      isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Failed to beep.")));
+      isolate->ThrowException(Exception::Error(String::NewFromUtf8(isolate, "Failed to beep.").ToLocalChecked()));
     }
   }
   else
   {
-    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Incorrect number of arguments.")));
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Incorrect number of arguments.").ToLocalChecked()));
   }
   return;
 }
